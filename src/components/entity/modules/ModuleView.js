4ea03a069e8b8f3a3fa956ee02f6ede4a68837fc
Joao Herzog <joaovitor_herzog@hotmail.com>
2024-12-10 07:51:04 +0000

    Activity 7 - Creating button and button tray
    Creating button and button tray
    -------------------------------
    
    1. Creating Button and ButtonTray components
    
    2. Modifying button styles dynamically
    
    3. Adding icons to buttons
    
    4. Creating icons

--- src/components/entity/modules/ModuleView.js
+++ src/components/entity/modules/ModuleView.js
@@ -1,5 +1,7 @@
-import {StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import FullWidthImage from "react-native-fullwidth-image";
+import Icons from "../../UI/Icons";
+import { Button, ButtonTray } from "../../UI/Button";
 
 const ModuleView = ({ module }) => {
   // Initialisations ---------------------
@@ -8,22 +10,27 @@ const ModuleView = ({ module }) => {
   // View --------------------------------
   return (
     <View style={styles.container}>
-    <FullWidthImage
-      source={{ uri: module.ModuleImage }}
-      style={styles.image}
-    />
+      <FullWidthImage source={{ uri: module.ModuleImage }} style={styles.image} />
+      <View style={styles.infoTray}>
+        <Text style={styles.boldText}>
+          {module.ModuleCode} {module.ModuleName}
+        </Text>
+        <Text style={styles.text}>Level {module.ModuleLevel}</Text>
+        <Text style={styles.text}>
+          {module.ModuleLeaderName} <Text style={styles.dimText}>(Module Leader)</Text>
+        </Text>
+      </View>
 
-    <View style={styles.infoTray}>
-      <Text style={styles.boldText}>
-        {module.ModuleCode} {module.ModuleName}
-      </Text>
-      <Text style={styles.text}>Level {module.ModuleLevel}</Text>
-      <Text style={styles.text}>
-        {module.ModuleLeaderName}{" "}
-        <Text style={styles.dimText}>(Module Leader)</Text>
-      </Text>
+      <ButtonTray>
+        <Button icon={<Icons.Edit/>} label='Modify' />
+        <Button
+          icon={<Icons.Delete/>}
+          label='Delete'
+          styleButton={{ backgroundColor: 'mistyrose' }}
+          styleLabel={{ color: 'red' }}
+        />
+      </ButtonTray>
     </View>
-  </View>
   );
 };
 
